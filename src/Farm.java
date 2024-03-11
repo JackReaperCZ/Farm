7ff9f5ad055ba49ca8bfce18534e6ab95f863bc1
JackReaperCZ <domcapour@seznam.cz>
2024-03-11 08:33:31 +0100

    Added farm harvest logic

--- src/Farm.java
+++ src/Farm.java
@@ -1,6 +1,7 @@
 import Flowers.Flower;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Farm {
     private double freeSpace;
@@ -17,7 +18,22 @@ public class Farm {
         } else {
             plantedFlowers.add(flower);
             freeSpace-=flower.getNeededArea();
+            System.out.println("Flower planted successfully.");
             return true;
         }
+    }
+
+    public void harvest(){
+        for (Flower f: plantedFlowers) {
+            double grownChance = f.getChanceOfGrowth();
+            Random r = new Random();
+            double chance = r.nextDouble(100);
+            if (chance<=grownChance){
+                System.out.println("You have harvested : "  + f + " successfully.");
+            } else {
+                System.out.println("You failed to harvested : "  + f + " successfully.");
+            }
+        }
+        plantedFlowers = new ArrayList<>();
     }
 }
