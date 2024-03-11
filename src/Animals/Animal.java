f4bddfd6219663d09eeea0e4bc5d6e95798b3623
JackReaperCZ <domcapour@seznam.cz>
2024-03-11 08:08:57 +0100

    Added class Sunflower.java

---
+++ src/Animals/Animal.java
@@ -0,0 +1,11 @@
+package Animals;
+
+public abstract class Animal {
+
+    protected String type;
+    protected String name;
+    protected Size size;
+    protected String specialCare;
+
+
+}
---
+++ src/Flowers/Flower.java
@@ -0,0 +1,12 @@
+package Flowers;
+
+public abstract class Flower {
+
+    protected String name;
+    protected double price;
+    protected double neededArea;
+    protected double chanceOfGrowth;
+
+
+
+}
